522d9d8f3aa8bb01beceb7100ea8fa074d76232e
Cbodtorf <cbodtorf@gmail.com>
2016-09-24 15:33:00 -0400

    updated the inventory routes with the new data structure.

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -14,6 +14,7 @@ exports.register = (server, options, next) => {
   *  ROUTES
   ***********************/
   server.route({
+    // find ALL inventory
     method: 'GET',
     path: '/inventory',
 
@@ -25,6 +26,118 @@ exports.register = (server, options, next) => {
           reply(data)
       })
     }
+  })
+
+  server.route({
+    // find ONE beer
+    method: 'GET',
+    path: '/inventory/{id}',
+
+    handler(request, reply) {
+      db.inventory.findOne(
+        {_id: request.params.id},
+        (err, data) => {
+
+        if (err) { return reply(Boom.wrap(err, 'Internal MongoDB error')) }
+        if (!data) { return reply(Boom.notFound()) }
+
+        reply(data)
+      })
+
+    }
+  })
+
+  server.route({
+    // save ONE beer
+    method: 'POST',
+    path: '/inventory',
+
+    handler(requet, reply) {
+      const beer = request.payload
+      //create an id
+      beer._id = Uuid.v1()
+
+      db.inventory.save(account, (err, result) => {
+
+        if (err) { return reply(Boom.wrap(err, 'Internal MongoDB error')) }
+
+        reply(account)
+      })
+
+    },
+    config: {
+      validate: {
+        payload: {
+
+          name: Joi.string().min(10).max(50).required(),
+          brewery: Joi.string().min(10).max(50).required(),
+          beerType: Joi.string().min(1).max(50).required(),
+          description: Joi.string().min(1).max(150).required(),
+          qtyCases: Joi.number().required(),
+          qtySixtels: Joi.number().required(),
+          qtyHalfBarrels: Joi.number().required(),
+          unitPrice: Joi.number().required(),
+          distributer: Joi.string().min(10).max(50).required()
+
+        }
+      }
+    }
+  })
+
+  server.route({
+    // update ONE beer
+    method: 'PATCH',
+    path: '/inventory/{id}',
+
+    handler(requet, reply) {
+
+      db.inventory.update(
+        {_id: request.params.id},
+        {$set: request.payload},
+        (err, result) => {
+
+          if (err) { return reply(Boom.wrap(err, 'Internal MongoDB error')) }
+          if (result.n === 0) { return reply(Boom.notFound()) }
+
+          reply().code(204);
+      })
+
+    },
+    config: {
+      validate: {
+        payload: Joi.object({
+
+          name: Joi.string().min(10).max(50).optional(),
+          brewery: Joi.string().min(10).max(50).optional(),
+          beerType: Joi.string().min(1).max(50).optional(),
+          description: Joi.string().min(1).max(150).optional(),
+          qtyCases: Joi.number().optional(),
+          qtySixtels: Joi.number().optional(),
+          qtyHalfBarrels: Joi.number().optional(),
+          unitPrice: Joi.number().optional(),
+          distributer: Joi.string().min(10).max(50).optional()
+
+        }).required().min(1)
+      }
+    }
+  })
+
+  server.route({
+      // remove ONE beer
+      method: 'DELETE',
+      path: '/inventory/{id}',
+      handler(request, reply) {
+
+          db.inventory.remove(
+            {id: request.params.id},
+            (err, result) => {
+
+              if (err) { return reply(Boom.wrap(err, 'Internal MongoDB error')) }
+              if (result.n === 0) { return reply(Boom.notFound()) }
+
+              reply().code(204);
+          })
+      }
   })
 
 
